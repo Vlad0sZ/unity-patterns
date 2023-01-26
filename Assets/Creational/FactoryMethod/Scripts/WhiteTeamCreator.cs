01ea37f0b35e726f9ce9721eb42f8caeaad7ca4d
Vladislav <vlad0s74chel@gmail.com>
2023-01-26 16:20:23 +0500

    Add factorymethod

---
+++ Assets/Creational/FactoryMethod/Scripts/WhiteTeamCreator.cs
@@ -0,0 +1,17 @@
+﻿using Creational.SingletonPattern.Scripts;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Creational.FactoryMethod.Scripts
+{
+    // создатель белых фигур на доске
+    public class WhiteTeamCreator : MonoBehaviour
+    {
+        public Unit whiteUnitPrefab;
+
+        public Unit CreateUnit(Vector3 position)
+        {
+            return Instantiate(whiteUnitPrefab, position, Quaternion.identity);
+        }
+    }
+}
